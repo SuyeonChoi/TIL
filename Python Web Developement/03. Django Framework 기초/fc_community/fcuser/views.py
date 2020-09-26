fa1db6f2c9bdc80d96e194d04867a291894ef103
SuyeonChoi <tndus970120@gmail.com>
2020-09-26 23:37:51 +0900

    [Python]Django Framework기초-로그인:Django Form

--- Python Web Developement/03. Django Framework 기초/fc_community/fcuser/views.py
+++ Python Web Developement/03. Django Framework 기초/fc_community/fcuser/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
-
+from .forms import LoginForm
 # Create your views here.
 
 def home(request):
@@ -20,24 +20,14 @@ def logout(request):
 
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password',None)
-
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야합니다.'
-        else:
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password, fcuser.password):
-                request.session['user'] = fcuser.id
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호가 틀렸습니다.'
-
-        return render(request, 'login.html', res_data)
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
 
 def register(request):
     if request.method == 'GET':
