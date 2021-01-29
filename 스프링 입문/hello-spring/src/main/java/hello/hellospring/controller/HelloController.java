da393cf73fb29a3f258b8e56f73a41e2353bd8a4
SuyeonChoi <tndus970120@gmail.com>
2021-01-29 23:55:36 +0900

    [스프링 입문]스프링 웹 개발 기초-API

--- 스프링 입문/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ 스프링 입문/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,18 +4,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 //Controller Annotation 기입 필수
 @Controller
 public class HelloController {
     @GetMapping("hello")
-    public String hello(Model model){
+    public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    //MVC, 템플릿 엔진
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    //API 방식
+    @GetMapping("hello-string")
+    @ResponseBody //http의 body에 데이터 직접 전달. 뷰 X.
+    public String helloSTring(@RequestParam("name") String name) {
+        return "hello " + name; //hello spring
+    }
+
+    //API방식 - json방식(key-value구조)으로 데이터 전달
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
