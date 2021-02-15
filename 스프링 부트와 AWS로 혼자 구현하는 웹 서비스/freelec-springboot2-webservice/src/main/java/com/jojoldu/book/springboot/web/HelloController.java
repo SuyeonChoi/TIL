7c839affe737ad10a6f89f7f56747113cd988290
SuyeonChoi <tndus970120@gmail.com>
2021-02-16 01:18:09 +0900

    [스프링부트&AWS]2.4 HelloController코드를 롬복으로 전환

--- 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //json을 반환하는 컨트롤러로 생성
@@ -9,5 +11,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    //RequestParam: 외부의 api로 넘긴 파라미터를 가져옴
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
--- 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) //JUnit에 내장된 실행자 외 다른 실행자를 실행. SpringRunner 실행자 사용
 @WebMvcTest(controllers = HelloController.class) //Web(SpringMVC)에 집중
@@ -24,6 +24,22 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) //MockMvc를 통해 /hello 주소로 HTTP GET 요청
                 .andExpect(status().isOk()) //mvc.perform 결과 검증. HTTP header의 status 검증(200, 404, 500 등). isOk는 200인지 아닌지 검증
                 .andExpect(content().string(hello)); //mvc.perform 결과 검증. 응답 본문의 내용 검증. hello가 맞는지
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        //param: api 테스트 시 사용될 용청 파라미터 설정. String만 허용
+        //jsonPath: json 응답값을 필드별로 검증하는 메소
+        mvc.perform(
+                get("/hello/dto")
+                    .param("name", name)
+                    .param("amount", String.valueOf(amount)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name", is(name)))
+                    .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
 
