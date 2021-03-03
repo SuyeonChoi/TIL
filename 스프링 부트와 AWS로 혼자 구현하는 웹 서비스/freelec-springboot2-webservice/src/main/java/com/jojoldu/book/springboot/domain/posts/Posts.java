0e820b2e577ff48ca401e98dfb06633ffa197a54
SuyeonChoi <tndus970120@gmail.com>
2021-03-04 03:37:13 +0900

    [스프링부트&AWS]3.2 Spring Data JPA 적용

--- 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+// Entity 클래스에서는 Setter 생성 X
+// 대신 목적(이벤트)에 맞는 public 메소드로 값을 변경 
+
 @Getter //모든 필드의 Getter메소드 자동생성
 @NoArgsConstructor // 기본 생성자 자동 추가
 @Entity // 테이블과 링크될 클래스임을 명시
@@ -22,7 +25,7 @@ public class Posts { //실제 DB테이블과 매칭될 클래스
 
     private String author;
 
-    @Builder //클래스의 빌더 패턴 클래스 생성. 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포
+    @Builder //클래스의 빌더 패턴 클래스 생성. 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
--- 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+//Entity 클래스와 기본 Entity Repository(Interface)는 함께 위치(domain)
+//기본적인 CRUD 메소드 자동 생성
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 
 }
