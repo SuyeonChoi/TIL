3e4c41e1b4e307320618ae7c1ce56dafe12abb9a
SuyeonChoi <tndus970120@gmail.com>
2021-03-08 00:22:53 +0900

    [스프링부트&AWS]4.3 게시글 등록 화면 제작

---
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/main/resources/static/js/app/index.js
@@ -0,0 +1,32 @@
+//index 객체 안에서만 function이 유효하도록하여 다른 JS와 겹칠 위험 제거  
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+    },
+    save : function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+
+};
+
+main.init();
