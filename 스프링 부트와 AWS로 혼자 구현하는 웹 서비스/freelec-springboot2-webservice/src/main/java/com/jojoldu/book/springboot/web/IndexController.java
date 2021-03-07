70279365b03f960fc1194b7b4ad2e2ae067b7913
SuyeonChoi <tndus970120@gmail.com>
2021-03-07 23:50:11 +0900

    [스프링부트&AWS]4.2 기본 페이지 제작

---
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//머스테치에 URL 매핑
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
