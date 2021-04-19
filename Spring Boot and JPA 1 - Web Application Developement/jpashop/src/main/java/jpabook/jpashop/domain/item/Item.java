31ecb938130c44ba758f368eb2b7a80184d6f494
SuyeonChoi <tndus970120@gmail.com>
2021-04-20 02:25:52 +0900

    [스프링부트와JPA활용]상품 도메인 개발-상품 엔티티 개발(비즈니스 로직 추가)

--- Spring Boot and JPA 1 - Web Application Developement/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ Spring Boot and JPA 1 - Web Application Developement/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==//
+    // 엔티티 안에서 해결할 수 있다면 비즈니스 로직을 엔티티 안에 생성 (객체 지향적. 응집력)
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
