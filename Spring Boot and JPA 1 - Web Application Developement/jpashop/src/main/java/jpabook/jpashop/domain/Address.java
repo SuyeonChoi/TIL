931eefa11eceadfb149a776c22cd5bdd9453c7a0
SuyeonChoi <tndus970120@gmail.com>
2021-04-19 22:56:16 +0900

    [스프링부트와JPA활용1]도메인 분석 설계-엔티티 클래스 개발2

--- Spring Boot and JPA 1 - Web Application Developement/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ Spring Boot and JPA 1 - Web Application Developement/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,15 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+        //jpa 스펙상 만듦
+        //함부로 생성되면 안되므로 protected 
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
