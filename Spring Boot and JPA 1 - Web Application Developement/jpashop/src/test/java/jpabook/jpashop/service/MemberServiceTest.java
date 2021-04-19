cf8283bf40754e1ac8df062c256184771341b087
SuyeonChoi <tndus970120@gmail.com>
2021-04-20 02:11:18 +0900

    [스프링부트와JPA활용1]회원 도메인 개발-회원 기능 테스트

---
+++ Spring Boot and JPA 1 - Web Application Developement/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class) // jUnit실행시 spring과 함께 실행
+@SpringBootTest // spring boot 실행 상태에서 테스트 돌림
+@Transactional // 테스트 클래스에서 끝나면 롤백됨
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    //    @Rollback(false) -> db 반영 확인
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        //em.flush(); // 영속성 context의 변경 내용을 db에 반영
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); //예외가 발생해야한다!
+        //then
+        fail("예외가 발생해야한");
+    }
+
+}
