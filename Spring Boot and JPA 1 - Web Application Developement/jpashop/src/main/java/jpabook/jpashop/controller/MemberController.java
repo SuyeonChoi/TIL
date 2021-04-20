c8c416d44cdd2fd8bb5aac4beab183765027a1b7
SuyeonChoi <tndus970120@gmail.com>
2021-04-20 11:20:14 +0900

    [스프링부트와JPA활용1]웹 계층 개발-회원목록조회

--- Spring Boot and JPA 1 - Web Application Developement/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ Spring Boot and JPA 1 - Web Application Developement/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -39,5 +40,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        //api로 개발시에는 엔티티를 전달하면 안됨. 
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
