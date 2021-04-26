4434e6a9c3ec09b6e0b773908ce8176211d3407e
SuyeonChoi <tndus970120@gmail.com>
2021-04-26 21:05:59 +0900

    [스프링부트와JPA활용2]API개발 고급:지연로딩과 조회성능 최적화-V1. 엔티티 직접 노출

--- Spring Boot and JPA 1 - Web Application Developement/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ Spring Boot and JPA 1 - Web Application Developement/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,19 +1,23 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import jpabook.jpashop.domain.Order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+    }
 
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-		System.out.println("data = " + data);
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+//        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
 
-		SpringApplication.run(JpashopApplication.class, args);
-	}
-
+    }
 }
