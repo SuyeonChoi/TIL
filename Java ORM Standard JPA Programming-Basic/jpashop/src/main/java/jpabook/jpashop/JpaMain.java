27f07bdf49296d6bea65ac3c83ae15d48b091ce5
SuyeonChoi <tndus970120@gmail.com>
2021-07-26 10:02:21 +0900

    [자바ORM표준JPA프로그래밍기본]엔티티 매핑-실전예제1.요구사항 분석과 기본 매핑

---
+++ Java ORM Standard JPA Programming-Basic/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); //로딩 시점에 한개만
+
+        EntityManager em = emf.createEntityManager(); // 요청이 올때마다 실행
+
+        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 DB transaction안에서 이루어져야함
+        tx.begin();
+
+        try {
+
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+
+            tx.commit(); // 필수
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
