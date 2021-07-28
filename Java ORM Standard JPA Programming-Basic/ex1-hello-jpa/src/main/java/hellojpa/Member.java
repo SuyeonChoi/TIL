ac0bc129ae62f79dec933fd99da1252676b3d5d6
SuyeonChoi <tndus970120@gmail.com>
2021-07-29 00:13:52 +0900

    [자바ORM표준JPA프로그래밍기본]다양한 연관관계 매핑-다대다[N:M]

--- Java ORM Standard JPA Programming-Basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ Java ORM Standard JPA Programming-Basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,5 +1,8 @@
 package hellojpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.*;
 
 @Entity
@@ -19,6 +22,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
