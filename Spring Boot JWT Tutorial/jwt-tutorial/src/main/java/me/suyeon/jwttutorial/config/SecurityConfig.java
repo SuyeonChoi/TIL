3919138a037d467a58c0f1f1c701f6888b154474
SuyeonChoi <tndus970120@gmail.com>
2021-07-30 01:37:46 +0900

    [Spring Boot JWT Tutorial]Security 설정, Data 설정

---
+++ Spring Boot JWT Tutorial/jwt-tutorial/src/main/java/me/suyeon/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package me.suyeon.jwttutorial.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity //기본적인 웹 보안 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+			.ignoring()
+			.antMatchers(
+				"/h2-console/**"
+				, "favicon.ico"
+			);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/api/hello").permitAll()
+			.anyRequest().authenticated();
+	}
+}
